3568998f7ebb22c9be253501028a15f4dbcc7516
Asif <asif16907@gmail.com>
2024-11-13 23:35:04 +0530

    Implemented Orderbooking

--- pageObject/cart.js
+++ pageObject/cart.js
@@ -1,19 +1,116 @@
+const { count } = require("console");
+
 exports.CartProcess = class CartProcess {
+  constructor(page) {
+    this.page = page;
+    this.email = page.getByRole("textbox", { name: "Email" });
+    this.password = page.getByRole("textbox", { name: "Password" });
+    this.loginButton = page.getByRole("button", { name: "Log in" });
+    this.searchBox = page.locator("#small-searchterms");
+    this.shoppingCart = page
+      .getByRole("link", { name: "Shopping cart" })
+      .first();
+    this.addToCartButton = page
+      .getByRole("button", { name: "Add to Cart" })
+      .first();
+    this.tnc = page.locator("#termsofservice");
+    this.checkoutButton = page.getByRole("button", { name: "Checkout" });
+    this.country = page.getByRole("combobox", { name: "Country:" });
+    this.state = page.getByRole("combobox", { name: "State / province:" });
 
-    constructor(page) {
-        this.page = page;
-        this.email = page.getByRole('textbox', { name: 'Email' })
-        this.password = page.getByRole('textbox', { name: 'Password' })
-        this.loginButton = this.page.getByRole('button', { name: 'Log in' });
-    }
+    this.city = page.getByRole("textbox", { name: "City:" })
+    this.pincode = page.getByRole("textbox", { name: "Zip / postal code:" });
+    this.phoneNumber = page.getByRole("textbox", { name: "Phone number:" });
+    this.address = page.getByRole("textbox", { name: "Address 1:" });
+    this.continueButton = page.getByRole("button", {
+      name: "Continue",
+      exact: true,
+    });
+    this.confirmbutton = page.getByRole("button", {
+      name: "Confirm",
+      exact: true,
+    });
+  }
 
-    async goto(){
-        await this.page.goto('https://demowebshop.tricentis.com/');
-    }
-    
-    async loginMethod(email, password) {
-        await this.email.fill(email)
-        await this.password.fill(password)
-        await this.loginButton.click()
-    }
-}
+  async goto() {
+    await this.page.goto("https://demowebshop.tricentis.com/");
+  }
+
+  async loginMethod(email, password) {
+    await this.email.fill(email);
+    await this.password.fill(password);
+    await this.loginButton.click();
+  }
+  async loginNav() {
+    await this.page.getByRole("link", { name: "Log in" }).click();
+  }
+
+  async searchItem(item) {
+    await this.searchBox.fill(item);
+    await this.page.getByRole("button", { name: "Search" }).click();
+  }
+
+  async openItem(item) {
+    await this.page.getByRole("link", { name: `${item}`, exact: true }).click();
+  }
+
+  async addToCart() {
+    await this.addToCartButton.click();
+  }
+
+  async gotoShoppingCart() {
+    await this.shoppingCart.click();
+  }
+
+  async checkTNC() {
+    await this.tnc.click();
+  }
+  async fillCountry(country) {
+    await this.country.selectOption({ value: `${country}` });
+  }
+
+  async fillState(state) {
+    await this.state.selectOption({ value: `${state}` });
+  }
+
+  async fillCity(city) {
+    await this.city.fill(city);
+  }
+
+  async fillAddress(address) {
+    await this.address.fill(address);
+  }
+  async fillPinCode(pin) {
+    await this.pincode.fill(pin);
+  }
+  async fillPhone(phone) {
+    await this.phoneNumber.fill(phone);
+  }
+
+  async checkout() {
+    await this.checkoutButton.click();
+  }
+
+  async addnewAddress() {
+    await this.page
+      .getByRole("combobox", {
+        name: "Select a billing address from your address book or enter a new address.",
+      })
+      .selectOption({ label: "New Address" });
+  }
+
+  async continueBooking() {
+    await this.continueButton.click();
+  }
+  async confirmBooking() {
+    await this.confirmbutton.click();
+  }
+
+  async deliveryType(deliveryMode) {
+    await this.page.getByLabel(`${deliveryMode}`).check();
+  }
+
+  async paymentType(paymentMode) {
+    await this.page.getByLabel(`${paymentMode}`).check();
+  }
+};
--- tests/cart.spec.js
+++ tests/cart.spec.js
@@ -1,37 +1,60 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import { CartProcess } from "../pageObject/cart";
 
-import { CartProcess } from '../pageObject/cart';
 
-test('Ordering Product', async ({ page }) => {
+test("Ordering Product", async ({ page }) => {
   const cart = new CartProcess(page)
-  cart.goto()
-  await page.getByRole('link', { name: 'Log in' }).click();
+  await cart.goto()
+  await cart.loginNav()
+  await cart.loginMethod('asif.testing@gmail.com','123456')
 
-  await cart.loginMethod('asif.testing@gmail.com', '123456')
-  await expect(page.getByText('Welcome to our store')).toBeVisible();
+  await expect(page.getByText("Welcome to our store")).toBeVisible();
 
-  // await page.goto('https://demowebshop.tricentis.com/ ');
-  // await expect(page).toHaveTitle(/Demo Web Shop/);
- 
-  // await page.getByRole('textbox', { name: 'Email'}).fill("asif.testing@gmail.com")
-  // await page.getByRole('textbox', { name: 'Password'}).fill("123456")
- 
-  // await page.getByRole('button', { name: 'Log in' }).click();
-
-  await page.locator('#small-searchterms').fill('Computer')
-  await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByRole('link', { name: 'Build your own cheap computer', exact: true  }).click();
-  await page.getByRole('button', { name: 'Add to Cart' }).first().click();
-  await page.getByRole('link', { name: 'Shopping cart' }).first().click();
-  await page.locator('#termsofservice').click()
-  await page.getByRole('button', { name: 'Checkout' }).click();
-  await expect(page.getByRole('heading', { name: 'Shopping cart' })).toBeVisible();
-
+  await cart.searchItem('Computer')
+  await cart.openItem("Build your own cheap computer")
+  await cart.addToCart()
+  await cart.gotoShoppingCart()
+  await cart.fillCountry('41')
+  await cart.fillState('0')
+  await cart.fillPinCode('110013')  
+  await cart.checkTNC()
+  await cart.checkout()
   
+  await cart.addnewAddress()
 
+  await cart.fillCountry('41')
+  await cart.fillState('0')
+  await cart.fillCity('New Delhi')
+  await cart.fillAddress('NZM')
+  await cart.fillPinCode('110013')
+  await cart.fillPhone('1029387465')
 
+  await cart.continueBooking()
+  await page.waitForTimeout(1000);
 
+  await cart.continueBooking()
 
+  await cart.deliveryType("Next Day Air")
 
+  await cart.continueBooking()
 
-})
+  await cart.paymentType("Cash on Delivery")
+
+  await cart.continueBooking()
+  await expect(page.getByText("You will pay by COD")).toBeVisible();
+
+  await cart.continueBooking()
+
+  await cart.confirmBooking()
+
+  await expect(
+    page.getByText("Your order has been successfully processed!")
+  ).toBeVisible();
+
+  await expect(page.getByText("Order number")).toBeVisible();
+  const ordernumber = (await page.getByText("Order number").innerText()).trim();
+  console.log(ordernumber);
+
+  await cart.continueBooking()
+
+});
